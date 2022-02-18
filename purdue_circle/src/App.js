91b9e7cb8b1094c738efb4eff762b8d1af347e0d
riyaverma11 <verma108@purdue.edu>
2022-02-18 16:45:53 -0500

    creating links to login and register page

--- purdue_circle/src/App.js
+++ purdue_circle/src/App.js
@@ -1,20 +1,36 @@
+//import logo from './logo.svg';
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import "./App.css";
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
+import './App.css';
 import Routes from "./Routes";
 
 function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <Navbar.Brand className="font-weight-bold text-muted">
-          P u r d u e C i r c l e
-        </Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand className="font-weight-bold text-muted">
+            P u r d u e C i r c l e
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Register</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
   );
 }
+
 
 export default App;
