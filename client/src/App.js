320ecb53d59b17dbf1957c2a98175bce1d9c8e0e
amritavivek <vivekana@purdue.edu>
2022-02-24 17:12:09 -0500

    added home page to client, updated register ui files for better coding style

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import Home from "./pages/home/home";
 import Login from "./pages/login/login";
 import Register from "./pages/register/register";
+import {
+  BrowserRouter as Router, 
+  Switch, 
+  Route, 
+  Link
+} from "react-router-dom";
 
 
 function App() {
   return (
-    <Register/>
+    <Router>
+      <Switch>
+        <Route path="/ "> </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
--- client/src/pages/register/register.js
+++ client/src/pages/register/register.js
@@ -2,21 +2,21 @@ import "./register.css"
 
 export default function Register() {
     return (
-        <div className="login">
-            <div className="loginWrapper">
-                <div className="loginLeft">
-                    <h3 className="loginLogo">PurdueCircle</h3>
-                    <span className="loginDesc">Connect with others at Purdue</span>
+        <div className="register">
+            <div className="registerWrapper">
+                <div className="registerLeft">
+                    <h3 className="registerLogo">PurdueCircle</h3>
+                    <span className="registerDesc">Connect with others at Purdue</span>
                 </div>
-                <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder="Email" className="loginInput"/>
-                        <input placeholder="Username" className="loginInput"/>
-                        <input placeholder="Password" className="loginInput"/>
-                        <input placeholder="Confirm Password" className="loginInput"/>
-                        <input placeholder="Age" className="loginInput"/>
-                        <button className="loginButton">Sign In</button>
-                        <button className="loginRegisterButton">Log In</button>
+                <div className="registerRight">
+                    <div className="registerBox">
+                        <input placeholder="Email" className="RegisterInput"/>
+                        <input placeholder="Username" className="RegisterInput"/>
+                        <input placeholder="Password" className="RegisterInput"/>
+                        <input placeholder="Confirm Password" className="RegisterInput"/>
+                        <input placeholder="Age" className="RegisterInput"/>
+                        <button className="RegisterButton">Sign In</button>
+                        <button className="registerLoginButton">Log In</button>
 
                     </div>
                 </div>
