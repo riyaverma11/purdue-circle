f980241fce46494d5054918e9f826590a350dc1c
amritavivek <vivekana@purdue.edu>
2022-02-25 00:20:37 -0500

    added post ui

--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./Feed.css";
+import userProfileImg from "../riya.jpeg"
 
 export default function Feed() {
     return (
-        <div className="Feed">
-                <div className="lander">
-                    <h1>There are no posts!</h1>
-                </div>
-        </div>
-    );
+      <div className="share">
+        <div className="shareWrapper">
+          <div className="shareTop">
+          <img className="shareProfileImg" src="../riya.jpeg" alt="" />
+            <input placeholder="What's on your mind?" className="shareInput"/>
+            <input placeholder="Topic" className="shareInput"/>
+          </div>
+            <hr className="shareHr"/>
 
-}
+          <div className="shareBottom">
+             <button className="uploadButton">Upload Image</button>
+              <button className="shareButton">Share</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
