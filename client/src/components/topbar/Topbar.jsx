9066f50a99fc1f5c9cf0da45e9915bc9b0bce14a
riyaverma11 <verma108@purdue.edu>
2022-03-28 23:18:31 -0400

    topbar BEFORE style changes

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,9 +1,47 @@
-import "./topbar.css"
+import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Topbar() {
-    return (
-        <div>
-            topbar
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Lamasocial</span>
+        </Link>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
         </div>
-    )
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        {/*
+        <Link to={`/profile/${user.username}`}>
+          <img
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+            className="topbarImg"
+          />
+        </Link>
+        */}
+      </div>
+    </div>
+  );
 }
