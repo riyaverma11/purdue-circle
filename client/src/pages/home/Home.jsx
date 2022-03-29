ecfe6783a217ee9fde451382204a30d55487308e
riyaverma11 <verma108@purdue.edu>
2022-03-29 00:05:02 -0400

    added info to the feed files

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
 import Topbar from "../../components/topbar/Topbar"
+import Feed from "../../components/feed/Feed"
 import "./home.css"
 
 export default function Home() {
     return (
-    <div> 
-        <Topbar/>    
-    </div>
+        <> 
+            <Topbar/>
+            <div className="homeContainer">
+                <Feed/> 
+            </div>
+        </>
         
-        
-    )
+    );
 }
