c13532f0ef985fa90bb748ec6e83bb6ac940e2ac
riyaverma11 <verma108@purdue.edu>
2022-03-31 01:50:09 -0400

    added / to line 17 in Feed.jsx --> shows all the user's posts on their profile

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -2,19 +2,6 @@ import { useContext, useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-/*
-export default function Feed () {
-
-    return (
-
-        <div className="feed">
-            <div className="feedWrapper">
-                <Share/>
-            </div>
-        </div>
-    )
-}
-*/
 import axios from "axios";
 import { AuthContext } from "../../context/AuthContext";
 
@@ -24,9 +11,10 @@ export default function Feed({ username }) {
 
   useEffect(() => {
     const fetchPosts = async () => {
+      //console.log(user);
       const res = username
         ? await axios.get("/posts/profile/" + username)
-        : await axios.get("posts/timeline/" + user._id);
+        : await axios.get("/posts/timeline/" + user._id);
       setPosts(
         res.data.sort((p1, p2) => {
           return new Date(p2.createdAt) - new Date(p1.createdAt);
