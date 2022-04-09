262fb28098273bd5c7eaf2c80408973d7f1f210f
riyaverma11 <verma108@purdue.edu>
2022-04-09 14:18:28 -0400

    created Theme.js and added color palette

---
+++ client/src/components/Theme.js
@@ -0,0 +1,13 @@
+//here we've defined and exported lightTheme and darkTheme objects with distinct color variables
+export const lightTheme = {
+    body: '#FFF',
+    text: '#363537',
+    toggleBorder: '#FFF',
+    background: '#363537',
+}
+export const darkTheme = {
+    body: '#363537',
+    text: '#FAFAFA',
+    toggleBorder: '#6B8096',
+    background: '#999',
+}
