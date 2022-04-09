7249b9847cd0c3a214e3785e184fbee79a7fee6f
riyaverma11 <verma108@purdue.edu>
2022-04-09 14:23:55 -0400

    updated App.js to reflect the addition of the light/dark mode button

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,22 @@ import EditProfile from "./pages/profile/EditProfile";
 import Register from "./pages/register/Register";
 import Logout from "./pages/logout/Logout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./components/globalStyle";
+import { lightTheme, darkTheme } from "./components/Theme"
 
 function App() {
+	const [theme, setTheme] = useState('light');
+	const themeToggler = () => {
+	  theme === 'light' ? setTheme('dark') : setTheme('light')
+  	}
+	
 	return (
+		<ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+		<>
+		<GlobalStyles/>
+		<button onClick={themeToggler}>Switch Theme</button>
 		<Router>
 			<Switch>
 				<Route exact path="/">
@@ -34,6 +47,8 @@ function App() {
 				</Route>
 			</Switch>
 		</Router>
+		</>
+    </ThemeProvider>
 	);
 }
 
