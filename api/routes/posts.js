c8df564866cd6f76540e9a525300a807877c85bf
Bhawin Mittal <bmittal@purdue.edu>
2022-04-20 20:16:45 -0400

    Implemented commenting (frontend + backend)

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -5,133 +5,163 @@ const User = require("../models/User");
 //create a post
 
 router.post("/", async (req, res) => {
-  const newPost = new Post(req.body);
-  console.log(newPost);
-  const topicName = await User.find({username: req.body.topic});
+	const newPost = new Post(req.body);
+	console.log(newPost);
+	const topicName = await User.find({ username: req.body.topic });
 
-  
-  if(!req.body.topic){
-    topicName[0] = "";
-  }
-  
-  //console.log(String(topicName[0]._id));
-  const newPost1 = {
-    userId: req.body.userId,
-    topic: String(topicName[0]._id),
-    desc: req.body.desc,
-    img: req.body.img,
-    likes: req.body.likes
-  };
+	if (!req.body.topic) {
+		topicName[0] = "";
+	}
 
-  const newPost1Obj = new Post(newPost1)
-  console.log(newPost1Obj)
-  try {
-    const savedPost = await newPost1Obj.save();
-    res.status(200).json(savedPost);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	//console.log(String(topicName[0]._id));
+	const newPost1 = {
+		userId: req.body.userId,
+		topic: String(topicName[0]._id),
+		desc: req.body.desc,
+		img: req.body.img,
+		likes: req.body.likes,
+		comments: req.body.comments
+	};
+
+	const newPost1Obj = new Post(newPost1);
+	console.log(newPost1Obj);
+	try {
+		const savedPost = await newPost1Obj.save();
+		res.status(200).json(savedPost);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 //update a post
 
 router.put("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.userId === req.body.userId) {
-      await post.updateOne({ $set: req.body });
-      res.status(200).json("the post has been updated");
-    } else {
-      res.status(403).json("you can update only your post");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		const post = await Post.findById(req.params.id);
+		if (post.userId === req.body.userId) {
+			await post.updateOne({ $set: req.body });
+			res.status(200).json("the post has been updated");
+		} else {
+			res.status(403).json("you can update only your post");
+		}
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 //delete a post
 
 router.delete("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.userId === req.body.userId) {
-      await post.deleteOne();
-      res.status(200).json("the post has been deleted");
-    } else {
-      res.status(403).json("you can delete only your post");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		const post = await Post.findById(req.params.id);
+		if (post.userId === req.body.userId) {
+			await post.deleteOne();
+			res.status(200).json("the post has been deleted");
+		} else {
+			res.status(403).json("you can delete only your post");
+		}
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 //like / dislike a post
 
 router.put("/:id/like", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (!post.likes.includes(req.body.userId)) {
-      await post.updateOne({ $push: { likes: req.body.userId } });
-      res.status(200).json("The post has been liked");
-    } else {
-      await post.updateOne({ $pull: { likes: req.body.userId } });
-      res.status(200).json("The post has been disliked");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post.likes.includes(req.body.userId)) {
+			await post.updateOne({ $push: { likes: req.body.userId } });
+			res.status(200).json("The post has been liked");
+		} else {
+			await post.updateOne({ $pull: { likes: req.body.userId } });
+			res.status(200).json("The post has been disliked");
+		}
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 //get a post
 
 router.get("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    res.status(200).json(post);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		const post = await Post.findById(req.params.id);
+		res.status(200).json(post);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 //get timeline posts
 
 router.get("/timeline/:userId", async (req, res) => {
-  try {
-    const currentUser = await User.findById(req.params.userId);
-    //const topic = await currentUser.findById("625de278b555fba6614a3ac1");
-    const userPosts = await Post.find({ userId: currentUser._id });
+	try {
+		const currentUser = await User.findById(req.params.userId);
+		//const topic = await currentUser.findById("625de278b555fba6614a3ac1");
+		const userPosts = await Post.find({ userId: currentUser._id });
 
-    const friendPosts = await Promise.all(
-      currentUser.followings.map((friendId) => {
-        return Post.find({ userId: friendId });
-      })
-    );
+		const friendPosts = await Promise.all(
+			currentUser.followings.map(friendId => {
+				return Post.find({ userId: friendId });
+			})
+		);
 
+		const topicPosts = await Promise.all(
+			currentUser.topicsFollowed.map(topicId => {
+				const post = Post.find({ topic: topicId });
+				return post;
+			})
+		);
 
-    const topicPosts = await Promise.all(
-        currentUser.topicsFollowed.map((topicId) => {
-
-          const post = Post.find({ topic: topicId});
-            return post;
-          
-       
-      })
-    );
-
-    res.status(200).json(userPosts.concat(...friendPosts).concat(...topicPosts));
-  } catch (err) {
-    res.status(500).json(err);
-  }
+		res
+			.status(200)
+			.json(userPosts.concat(...friendPosts).concat(...topicPosts));
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 //get user's all posts
 
 router.get("/profile/:username", async (req, res) => {
-  
-  try {
-    const user = await User.findOne({ username: req.params.username });
-    const posts = await Post.find({ userId: user._id });
-    const topicPosts = await Post.find({topic: user._id});
-    res.status(200).json(posts.concat(...topicPosts));
-  } catch (err) {
-    res.status(500).json("error in posts.js (routes)");
-  }
+	try {
+		const user = await User.findOne({ username: req.params.username });
+		const posts = await Post.find({ userId: user._id });
+		const topicPosts = await Post.find({ topic: user._id });
+		res.status(200).json(posts.concat(...topicPosts));
+	} catch (err) {
+		res.status(500).json("error in posts.js (routes)");
+	}
+});
+
+//post a comment
+
+router.put("/:postId/comment", async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.postId);
+		const commentObj = {
+			text: req.body.text,
+			username: req.body.username
+		};
+		await post.updateOne({ $push: { comments: commentObj } });
+		res.status(200).json("the comment has been successfully posted");
+		// if (post.userId === req.body.userId) {
+		//   await post.updateOne({ $set: req.body });
+		//   res.status(200).json("the post has been updated");
+		// } else {
+		//   res.status(403).json("you can update only your post");
+		// }
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+//get a post's comments
+
+router.get("/comments/:postId", async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.postId);
+		res.status(200).json(post.comments);
+	} catch (err) {
+		res.status(500).json("error in posts.js (routes)");
+	}
 });
 
 module.exports = router;
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -5,12 +5,29 @@ import axios from "axios";
 import { format } from "timeago.js";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+const Item = styled(Paper)(({ theme }) => ({
+	backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+	...theme.typography.body2,
+	padding: theme.spacing(1),
+	textAlign: "left",
+	color: theme.palette.text.secondary
+}));
 
 export default function Post({ post }) {
 	const [like, setLike] = useState(post.likes.length);
+	const [commentObjs, setCommentObjs] = useState(post.comments);
 	const [isLiked, setIsLiked] = useState(false);
 	const [user, setUser] = useState({});
 	const [topicName, setTopic] = useState();
+	const [showComments, setShowComments] = useState(false);
+	const [newComment, setNewComment] = useState("");
 	const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 	const { user: currentUser } = useContext(AuthContext);
 
@@ -26,14 +43,45 @@ export default function Post({ post }) {
 		fetchUser();
 	}, [post.userId]);
 
-	useEffect(()=> {
+	useEffect(() => {
 		const fetchTopicName = async () => {
 			const res = await axios.get(`/users?userId=${post.topic}`);
 			//console.log(res.data.username)
 			setTopic(res.data.username);
 		};
 		fetchTopicName();
-	}, [post.topic])
+	}, [post.topic]);
+
+	const showCommentsToggle = () => {
+		if (showComments) {
+			setShowComments(false);
+		} else {
+			setShowComments(true);
+		}
+	};
+
+	const commentPostHandler = async e => {
+		e.preventDefault();
+		console.log("comment post handler");
+		console.log(newComment);
+		console.log(post._id);
+		console.log(currentUser._id);
+		const newCommentObj = {
+			text: newComment,
+			username: currentUser.username
+		};
+		console.log(newCommentObj);
+		try {
+			await axios.put("/posts/" + post._id + "/comment", newCommentObj);
+			console.log("comment posted");
+			const res = await axios.get(`/posts/comments/${post._id}`);
+			setCommentObjs(res.data);
+			setNewComment("");
+			// window.location.reload();
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
 	const likeHandler = () => {
 		try {
@@ -45,12 +93,12 @@ export default function Post({ post }) {
 
 	const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 
-const renderText = txt =>
-  txt
-    .split(" ")
-    .map(part =>
-      URL_REGEX.test(part) ? <a href={part}>{part} </a> : part + " "
-    );
+	const renderText = txt =>
+		txt
+			.split(" ")
+			.map(part =>
+				URL_REGEX.test(part) ? <a href={part}>{part} </a> : part + " "
+			);
 
 	return (
 		<div className="post">
@@ -79,9 +127,8 @@ const renderText = txt =>
 				<div className="postCenter">
 					<span className="postText">{renderText(post.desc)}</span>
 					<img className="postImg" src={PF + post.img} alt="" />
-					
 				</div>
-				
+
 				{/*}
 				<div>
 					<span className="postTopic">{"#" + (post.topic)}</span>
@@ -90,29 +137,66 @@ const renderText = txt =>
 
 				<div className="postBottom">
 					<div className="postBottomLeft">
-
-					{(topicName) && 
-						<div>
-							<span className="postTopic">{"#" + (topicName)}</span>
-						</div>
-					}
+						{topicName && (
+							<div>
+								<span className="postTopic">{"#" + topicName}</span>
+							</div>
+						)}
 						<div className="shareOption">
-                    		<BookmarkAdd htmlColor="DarkGray" className="shareIcon" />
-                        		<span className="shareOptionText">Save Post</span>
-                		</div>
+							<BookmarkAdd htmlColor="DarkGray" className="shareIcon" />
+							<span className="shareOptionText">Save Post</span>
+						</div>
 						<img
 							className="likeIcon"
 							src={`${PF}like.png`}
 							onClick={likeHandler}
 							alt=""
 						/>
-						
+
 						<span className="postLikeCounter">{like} likes</span>
 					</div>
 					<div className="postBottomRight">
-						<span className="postCommentText">{post.comment} comments</span>
+						<span className="postCommentText" onClick={showCommentsToggle}>
+							{post.comment} comments
+						</span>
 					</div>
 				</div>
+				{showComments ? (
+					<div className="postBottomComments">
+						{/* <div className="commentCell"></div> */}
+						<div style={{ marginTop: "1rem" }}></div>
+						<Box sx={{ width: "100%" }}>
+							<Stack spacing={2}>
+								{commentObjs.map(c => (
+									<Item>
+										<small className="commentUsername">{c.username}</small>
+										<p className="commentText">{c.text}</p>
+									</Item>
+								))}
+							</Stack>
+						</Box>
+						<div style={{ marginTop: "1rem" }}></div>
+						<div className="commentFieldBox">
+							<TextField
+								id="standard-basic"
+								label="Enter your comment here..."
+								value={newComment}
+								onChange={e => {
+									setNewComment(e.target.value);
+								}}
+								variant="standard"
+								sx={{ width: "85%" }}
+							/>
+							<Button
+								variant="contained"
+								onClick={commentPostHandler}
+								style={{ margin: "1.5vh 1vw" }}
+							>
+								Submit
+							</Button>
+						</div>
+					</div>
+				) : null}
 			</div>
 		</div>
 	);
